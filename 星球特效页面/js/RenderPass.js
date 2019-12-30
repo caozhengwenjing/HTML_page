7e700a0bac7b1aa92e16098cc6fe6aec05fd89eb
caozhengwnejing <15934492677@163.com>
2019-12-30 09:39:02 +0800

    add

--- 星球特效页面/js/RenderPass.js
+++ 星球特效页面/js/RenderPass.js
@@ -2,62 +2,62 @@
  * @author alteredq / http://alteredqualia.com/
  */
 
-THREE.RenderPass = function ( scene, camera, overrideMaterial, clearColor, clearAlpha ) {
+THREE.RenderPass = function (scene, camera, overrideMaterial, clearColor, clearAlpha) {
 
-	THREE.Pass.call( this );
+    THREE.Pass.call(this);
 
-	this.scene = scene;
-	this.camera = camera;
+    this.scene = scene;
+    this.camera = camera;
 
-	this.overrideMaterial = overrideMaterial;
+    this.overrideMaterial = overrideMaterial;
 
-	this.clearColor = clearColor;
-	this.clearAlpha = ( clearAlpha !== undefined ) ? clearAlpha : 0;
+    this.clearColor = clearColor;
+    this.clearAlpha = (clearAlpha !== undefined) ? clearAlpha : 0;
 
-	this.clear = true;
-	this.clearDepth = false;
-	this.needsSwap = false;
+    this.clear = true;
+    this.clearDepth = false;
+    this.needsSwap = false;
 
 };
 
-THREE.RenderPass.prototype = Object.assign( Object.create( THREE.Pass.prototype ), {
+THREE.RenderPass.prototype = Object.assign(Object.create(THREE.Pass.prototype), {
 
-	constructor: THREE.RenderPass,
+    constructor: THREE.RenderPass,
 
-	render: function ( renderer, writeBuffer, readBuffer, delta, maskActive ) {
+    render: function (renderer, writeBuffer, readBuffer, delta, maskActive) {
 
-		var oldAutoClear = renderer.autoClear;
-		renderer.autoClear = false;
+        var oldAutoClear = renderer.autoClear;
+        renderer.autoClear = false;
 
-		this.scene.overrideMaterial = this.overrideMaterial;
+        this.scene.overrideMaterial = this.overrideMaterial;
 
-		var oldClearColor, oldClearAlpha;
+        var oldClearColor, oldClearAlpha;
 
-		if ( this.clearColor ) {
+        if (this.clearColor) {
 
-			oldClearColor = renderer.getClearColor().getHex();
-			oldClearAlpha = renderer.getClearAlpha();
+            oldClearColor = renderer.getClearColor().getHex();
+            oldClearAlpha = renderer.getClearAlpha();
 
-			renderer.setClearColor( this.clearColor, this.clearAlpha );
+            renderer.setClearColor(this.clearColor, this.clearAlpha);
 
-		}
+        }
 
-		if ( this.clearDepth ) {
+        if (this.clearDepth) {
 
-			renderer.clearDepth();
+            renderer.clearDepth();
 
-		}
+        }
 
-		renderer.render( this.scene, this.camera, this.renderToScreen ? null : readBuffer, this.clear );
+        renderer.render(this.scene, this.camera, this.renderToScreen ? null : readBuffer, this.clear);
 
-		if ( this.clearColor ) {
+        if (this.clearColor) {
 
-			renderer.setClearColor( oldClearColor, oldClearAlpha );
+            renderer.setClearColor(oldClearColor, oldClearAlpha);
 
-		}
+        }
 
-		this.scene.overrideMaterial = null;
-		renderer.autoClear = oldAutoClear;
-	}
+        this.scene.overrideMaterial = null;
+        renderer.autoClear = oldAutoClear;
+    }
 
-} );
+});
